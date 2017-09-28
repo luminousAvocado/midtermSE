c86c825af0f3b48def3a61749bf4abce81a725db
luminousAvocado <jamesckang@gmail.com>
2017-09-27 18:33:06 -0700

    first commit

---
+++ src/midtermSE/helloworld.java
@@ -0,0 +1,10 @@
+package midtermSE;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World! This is James Kang, from CS-3337-2, MW 6:00pm");
+	}
+
+}
